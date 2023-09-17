689597b592a9fad49bf879a37661fb71f4a6d2c2
Naruya Kondo <n-kondo@g.ecc.u-tokyo.ac.jp>
2023-09-18 01:46:55 +0900

    fix zero_grad in grad accum mode

--- eg3d/training/training_loop.py
+++ eg3d/training/training_loop.py
@@ -279,7 +279,8 @@ def training_loop(
                 phase.start_event.record(torch.cuda.current_stream(device))
 
             # Accumulate gradients.
-            phase.opt.zero_grad(set_to_none=True)
+            if phase.i == 0:
+                phase.opt.zero_grad(set_to_none=True)
             phase.module.requires_grad_(True)
             for real_img, real_c, gen_z, gen_c in zip(phase_real_img, phase_real_c, phase_gen_z, phase_gen_c):
                 loss.accumulate_gradients(phase=phase.name, real_img=real_img, real_c=real_c, gen_z=gen_z, gen_c=gen_c, gain=phase.interval, cur_nimg=cur_nimg)
